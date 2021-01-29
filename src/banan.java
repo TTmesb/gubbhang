600b7972e1e2551572d122696b5eda5a7ce9569b
TTmesb <TTmesb@users.noreply.github.com>
2021-01-29 10:52:40 +0100

    gissning

--- src/banan.java
+++ src/banan.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class banan {
     private static String[] ordlista = {"linus", "dennis", "live", "laugh", "hypernervokustiskadiafragmakontravibrationer", "kan", "pluto", "honung", "överdosering", "radiopejlindikator"};
     private static String word = ordlista[new Random().nextInt(ordlista.length)];
-    private static String ord = new String(new char[word.length()]).replace("\0","_l");
+    private static String ord = new String(new char[word.length()]).replace("\0", "_l");
     private static int försök = 0;
 
     public static void main(String[] args) {
@@ -14,19 +14,24 @@ public class banan {
             System.out.println("Gissa en bokstav");
             System.out.println(ord);
             String gissning = sc.next();
+            hang(gissning);
+        }
+        sc.close();
+        // Ord gissning, anger antalet försök man får och om det finns några ord kvar att gissa
     }
-}
 
-    public static void ap(String[] gissning) {
+
+    public static void hang(String gissning) {
         String ordet = "";
         for (int i = 0; i < word.length(); i++) {
-            if (word)
+            if (word.charAt(i) == gissning.charAt(0)) {
+                ord += gissning.charAt(0);
+            } else if (ordet.charAt(i) != '*') {
+                ord += gissning.charAt(0);
+            } else {
+                ord += '*';
+            }
         }
-
+        // gör så att man håller koll på antal gissningar,
     }
-
-
-
-
-
 }
